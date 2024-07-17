2c5e05ff0ec9a02d6660989c3d6083a45feb0e6a
Hasuki <denzfordev@gmail.com>
2024-07-17 09:55:23 +0700

    refactor

--- internal/model/response/json.go
+++ internal/model/response/json.go
@@ -1,11 +1,6 @@
 package response
 
 type (
-	//Json[T any] struct {
-	//	Message string `json:"message"`
-	//	Data    *T     `json:"data"`
-	//}
-
 	Json struct {
 		Message string      `json:"message"`
 		Data    interface{} `json:"data"`
